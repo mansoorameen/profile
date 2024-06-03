031286a1d83a7f24dad2b96a171b45d99bdd9c1c
mansoor <manzooravail@gmail.com>
2024-06-04 02:23:25 +0530

    fix: navbar links and its mobile view, open footer links in new tab

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const navToggle = document.querySelector('.nav-toggle')
-const links = document.querySelector('.links')
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-navToggle.addEventListener('click', function () {
-  links.classList.toggle('show-links')
-})
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
+// Get the current page URL
+var currentPage = window.location.pathname;
+
+// Loop through each link and check if its href matches the current page URL
+links.forEach(function (link) {
+  if (link.getAttribute("href") === currentPage) {
+    // Add link-active class to the current page link
+    link.classList.add("link-active");
+  }
+});
